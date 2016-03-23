3fb668587ad862cdbd5e4bf2e070a9fe48d576ce
Fantom <fantom@dhcppc0>
2016-03-23 14:59:01 +0200

    edit commit

--- project.py
+++ project.py
@@ -1,8 +1,11 @@
-from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+import logging
+import sys
+
+from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
+
 from database_setup import Base, Shop, Items
-import logging, sys
 
 engine = create_engine('sqlite:///shopitems.db')
 Base.metadata.bind = engine
@@ -16,31 +19,36 @@ app.logger.setLevel(logging.ERROR)
 
 
 @app.route('/')
-def Summery():
+def summery():
     return render_template('summery.html')
 
 
 @app.route('/GetAllShops/JSON')
-def GetAllShops():
+def get_all_hops():
     shops = session.query(Shop).all()
     return jsonify(Shop=[i.serialize for i in shops])
 
 
+@app.route('/GetShop/<int:shop_id>/JSON')
+def get_shop(shop_id):
+    shops = session.query(Shop).filter_by(id=shop_id)
+    return jsonify(Shop=[i.serialize for i in shops])
+
 
 @app.route('/GetShopItems/<int:shop_id>/JSON')
-def GetShopItemsJSON(shop_id):
+def get_shop_items_json(shop_id):
     items = session.query(Items).filter_by(shop_id=shop_id)
     return jsonify(Items=[i.serialize for i in items])
 
 
 @app.route('/GetItem/<int:item_id>/JSON')
-def GetItemJSON(item_id):
+def get_item_json(item_id):
     items = session.query(Items).filter_by(id=item_id)
     return jsonify(Items=[i.serialize for i in items])
 
 
 @app.route('/GetShopItems/<int:shop_id>/')
-def GetShopItems(shop_id):
+def get_shop_items(shop_id):
     shop = session.query(Shop).filter_by(id=shop_id).one()
     items = session.query(Items).filter_by(shop_id=shop_id)
     return render_template('menu.html', shop=shop, items=items)
@@ -48,12 +56,13 @@ def GetShopItems(shop_id):
 
 @app.route('/newShopItem/<int:shop_id>/', methods=['GET', 'POST'])
 # Task 1: Create route for newShopItem function here
-def newShopItem(shop_id):
+def new_shop_item(shop_id):
+    global new_item
     shop = session.query(Shop).filter_by(id=shop_id).one()
     if request.method == 'POST':
         if request.form['name']:
-            newItem = Items(name=request.form['name'], shop_id=shop_id)
-        session.add(newItem)
+            new_item = Items(name=request.form['name'], shop_id=shop_id)
+        session.add(new_item)
         session.commit()
         flash("New Item Added!!")
         return redirect(url_for('GetShopItems', shop_id=shop_id))
@@ -63,7 +72,7 @@ def newShopItem(shop_id):
 
 @app.route('/editShopItem/<int:shop_id>/<int:item_id>/', methods=['GET', 'POST'])
 # Task 2: Create route for editShopItem function here
-def editShopItem(shop_id, item_id):
+def edit_shop_item(shop_id, item_id):
     shop = session.query(Shop).filter_by(id=shop_id).one()
     item = session.query(Items).filter_by(id=item_id, shop_id=shop_id).one()
     if request.method == 'POST':
@@ -79,7 +88,7 @@ def editShopItem(shop_id, item_id):
 
 @app.route('/deleteShopItem/<int:shop_id>/<int:item_id>/', methods=['GET', 'POST'])
 # Task 3: Create route for deleteShopItem function here
-def deleteShopItem(shop_id, item_id):
+def delete_shop_item(shop_id, item_id):
     shop = session.query(Shop).filter_by(id=shop_id).one()
     item = session.query(Items).filter_by(id=item_id, shop_id=shop_id).one()
     if request.method == 'POST':
